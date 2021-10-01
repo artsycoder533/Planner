ce2d8d5709af27eb1f4337029e52b69637b3c6ef
Natasha <njohnson533@gmail.com>
2021-10-01 18:43:49 -0400

    todos.js
    created todos class and constructor

---
+++ src/scripts/todos.js
@@ -0,0 +1,11 @@
+class Todos {
+    constructor(title, description, dueDate, priority, id) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.id = id;
+    }
+
+    
+}
