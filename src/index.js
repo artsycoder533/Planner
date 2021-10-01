9698fb42ffafe23d8825640fd8e8240667dbf7fc
Natasha <njohnson533@gmail.com>
2021-09-30 23:48:58 -0400

    added webpack bundle, moved to dist folder

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-alert("it works!");
+import "./styles/style.scss";
+import Pic from "./assets/pic.jpg";
+
+function component() {
+    const element = document.createElement("div");
+    const myImage = new Image();
+    myImage.src = Pic;
+    const heading = document.createElement("h1");
+    heading.classList.add("heading");
+    heading.textContent = "It worked!!!!!";
+    element.appendChild(heading);
+    element.appendChild(myImage);
+    return element;
+}
+
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,47 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+	mode: "development",
 	entry: "./src/index.js",
 	output: {
-		filename: "main.js",
+		filename: "bundle.js",
 		path: path.resolve(__dirname, "dist"),
+		clean: true,
 	},
+	module: {
+		rules: [
+			{
+				test: /\.(sc|c)ss$/i,
+				use: [
+					MiniCssExtractPlugin.loader,
+					"css-loader",
+					{
+						loader: "sass-loader",
+						options: {
+							// Prefer `dart-sass`
+							implementation: require.resolve("sass"),
+						},
+					},
+				],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+		],
+	},
+	devtool: "inline-source-map",
+	devServer: {
+		static: "./dist",
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Planner",
+		}),
+		new MiniCssExtractPlugin({
+			linkType: false,
+		}),
+	],
 };
