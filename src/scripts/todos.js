3f988551d5e9b0d0ee20d94c8369fd947253ecdc
Natasha <njohnson533@gmail.com>
2021-10-02 02:26:27 -0400

    added getters and setters to Todos class

--- src/scripts/todos.js
+++ src/scripts/todos.js
@@ -7,5 +7,49 @@ class Todos {
         this.id = id;
     }
 
+    getTitle() {
+        return this.title;
+    }
+
+    setTitle(title) {
+        this.title = title;
+    }
+
+    getDescription() {
+        return this.description;
+    }
+
+    setDescription(description) {
+        this.description = description;
+    }
+
+    getDueDate() {
+        return this.dueDate;
+    }
+
+    setDueDate(dueDate) {
+        this.dueDate = dueDate;
+    }
     
+    getPriority() {
+        return this.priority;
+    }
+
+    setPriority(priority) {
+        this.priority = priority;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    setId(id) {
+        this.id = id;
+    }
+
+    deleteTodos(id) {
+        
+    }
 }
+
+const todo = new Todo("finish portoflio", "finish personal portfolio and upload to Github",);
