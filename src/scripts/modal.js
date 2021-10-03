0552548f0afd9652509a187730f31b1fa5a6a7fc
Natasha <njohnson533@gmail.com>
2021-10-02 21:25:34 -0400

    added modal.js
    imported createElementWithClass function

---
+++ src/scripts/modal.js
@@ -0,0 +1,9 @@
+import { createElementWithClass } from "./createUI.js";
+
+function renderModal() {
+    const section = createElementWithClass("section", "modal");
+    const div = createElementWithClass("div", "modal__container");
+
+}
+
+export { renderModal };
