40b740e24c202727ee33cdabe8302a4288b584dc
Natasha <njohnson533@gmail.com>
2021-10-04 19:06:26 -0400

    added id parameter to renderForm function

--- src/scripts/events.js
+++ src/scripts/events.js
@@ -2,18 +2,22 @@ function addEvents() {
     const addBtn = document.getElementById("add");
     const modal = document.querySelector(".modal");
     const closeBtn = document.querySelector(".form__closeBtn");
+    const createTodo = document.querySelector(".form__createBtn");
 
     addBtn.addEventListener("click", () => {
         modal.classList.add("show");
     });
 
-    // modal.addEventListener("click", () => {
-    //     modal.classList.remove("show");
-    // });
-
     closeBtn.addEventListener("click", () => {
         modal.classList.remove("show");
     });
+
+    createTodo.addEventListener("click", (e) => {
+        e.preventDefault();
+
+    });
 }
+
+
 
 export { addEvents };
--- src/scripts/form.js
+++ src/scripts/form.js
@@ -4,17 +4,17 @@ function renderForm() {
     const form = createElementWithClass("form", "modal__form");
     const closeBtn = createTextElement("button", "form__closeBtn","X");
     const titleLabel = createFormElement("label", "for", "title", "form__label", "Title:");
-    const titleInput = createFormElement("input", "type", "text", "form__input");
+    const titleInput = createFormElementWithId("input", "title", "type", "text", "form__input");
     const descriptionLabel = createFormElement("label", "for", "description", "form__label", "Description");
-    const descriptionInput = createFormElement("input", "type", "textarea", "form__description");
+    const descriptionInput = createFormElementWithId("input", "description", "type", "textarea", "form__description");
     const dueDateLabel = createFormElement("label", "for", "dueDate", "form__label", "Due Date:");
-    const dueDateInput = createFormElement("input", "type", "date", "form__input");
+    const dueDateInput = createFormElementWithId("input", "dueDate", "type", "date", "form__input");
     const priorityLabel = createFormElement("label", "for", "priority", "form__label", "Priority:");
-    const selectPriority = createFormElement("select", "name", "priority", "form__select");
+    const selectPriority = createFormElementWithId("select", "priority", "name", "priority", "form__select");
     const lowPriority = createFormOption("option", "value", "normal", "Normal");
     const highPriority = createFormOption("option", "value", "high", "High");
     const projectLabel = createFormElement("label", "for", "project", "form__label", "Project:");
-    const selectProject = createFormElement("select", "name", "project", "form__select");
+    const selectProject = createFormElementWithId("select", "project", "name", "project", "form__select");
     const projectName = createFormOption("option", "value", "default", "Default");
     const createBtn = createTextElement("button", "form__createBtn", "Create");
     
@@ -24,6 +24,15 @@ function renderForm() {
     return form;
 }
 
+function createFormElementWithId(type, id, attribute, attributeType, className, text) {
+    const element = document.createElement(type);
+    element.setAttribute(attribute, attributeType);
+    element.setAttribute("id", id);
+    element.classList.add(className);
+    element.textContent = text;
+    return element;
+
+}
 function createFormElement(type, attribute, attributeType, className, text) {
     const element = document.createElement(type);
     element.setAttribute(attribute, attributeType);
